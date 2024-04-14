3623a1e7469e80cff98716a875d1d944d5124ef3
Sungjae Park <123699102+spark0510@users.noreply.github.com>
2024-04-14 16:03:36 -0400

    Update nullstart.R

--- nullstart/nullstart.R
+++ nullstart/nullstart.R
@@ -1,6 +1,12 @@
 nullstart <- function() {
     # Example function that does nothing but log a short string
     # Can be used, for example, as a start function that triggers concurrent executions
-
+    
     faasr_log("nullstart successfully invoked")
+
+    a <- Sys.time()
+    readr::write_rds(a, "start_invoke.RDS")
+    FaaSr::faasr_put_file(remote_folder=paste0(.faasr$FaaSrLog, "/", .faasr$InvocationID), 
+                          remote_file="start_invoke.RDS", 
+                          local_file="start_invoke.RDS")
 }
